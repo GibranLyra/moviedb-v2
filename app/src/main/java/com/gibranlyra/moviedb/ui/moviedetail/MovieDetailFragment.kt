a030684c93da59e75c5eb7b3fcf53d1600f25362
Gibran Lyra <lyra.gibran@gmail.com>
2019-07-02 15:01:14 -0300

    added toolbar to MovieDetail

--- app/src/main/java/com/gibranlyra/moviedb/ui/moviedetail/MovieDetailFragment.kt
+++ app/src/main/java/com/gibranlyra/moviedb/ui/moviedetail/MovieDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -50,8 +51,14 @@ class MovieDetailFragment : Fragment() {
         initViewModel()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_movie_detail, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        view.findViewById<Toolbar>(R.id.fragmentMovieDetailToolbar).run {
+            setNavigationIcon(R.drawable.ic_arrow_back)
+            setNavigationOnClickListener { activity?.onBackPressed() }
+        }
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
